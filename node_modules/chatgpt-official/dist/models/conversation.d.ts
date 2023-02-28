6b59438e3692ce16d66d8c6b82073599b79d8ab8
iskry <refocuslucifer@protonmail.com>
2023-02-27 19:39:32 -0600

    first commit

---
+++ node_modules/chatgpt-official/dist/models/conversation.d.ts
@@ -0,0 +1,8 @@
+import Message from "./message.js";
+interface Conversation {
+    id: string;
+    messages: Message[];
+    userName: string;
+    lastActive?: number;
+}
+export default Conversation;
---
+++ node_modules/chatgpt-official/dist/models/conversation.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=conversation.js.map
