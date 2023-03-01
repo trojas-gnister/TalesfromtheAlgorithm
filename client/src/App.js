78ceaf8d7f60d27e8b36191d7cf4956a7e709e29
Jordan <jnbarringer96@gmail.com>
2023-03-01 12:06:38 -0600

    added start button, title, and dropdown options

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+function startGame(){
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Tales From the Algorithm
+        </h1>
+        <div id="startBtn">
+          <button onClick={startGame()}>
+            Start
+          </button>
+        </div>
+        <div id="genre-dropdown">
+          <label>What genre would you like your story to be?</label>
+          <select>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+            <option value="Romance">Romance</option>
+          </select>
+        </div>
+        <div id="character-name">
+          <label>Who would you like to be the main character of your story?</label>
+          <select>
+            <option value="shenron">Shenron</option>
+            <option value="miso">Miso</option>
+            <option value="nugget">Nugget</option>
+          </select>
+        </div>
+        <div id="character-type">
+          <label>What is your character's background?</label>
+          <select>
+            <option value="chef">Chef</option>
+            <option value="assassin">Assassin</option>
+            <option value="student">Student</option>
+          </select>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
