fc53a7e446ffd0d157e21829535ebd0e751611d9
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-03-01 21:56:09 -0500

    added Home and Layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
-function startGame(){
-
-}
-
 function App() {
-  return ();
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+    </>
+  );
 }
 
 export default App;
