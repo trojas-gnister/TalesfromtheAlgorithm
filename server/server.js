65f90dac86db81d6ef4efc6ea7a252b919e428b7
iskry <refocuslucifer@protonmail.com>
2023-03-02 20:14:33 -0600

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-require("dotenv").config();
-
-const { ChatGPT } = require("./config/api/ChatGPT.js");
-const apiKey = process.env.API_KEY;
-const test = "2213";
+require('dotenv').config();
+const apiKey = process.env.API_KEY
 
 async function startServer() {
   const app = express();
-  app.use(express.static("public"));
+  app.use(express.static('public'));
 
-  const prompt1 = "hello i am story man can you make story pleas";
-  let response = await ChatGPT.ask(`${prompt1}`);
+  let options = {
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    instructions: `You are ChatGPT, a large language model trained by OpenAI.`,
+    model: "text-davinci-003",
+    stop: "",
+  }
+
+  const { default: ChatGPT } = await import("chatgpt-official");
+
+  let bot = new ChatGPT(apiKey, options);
+
+  let response = await bot.ask("think of 5 funny and clever names about generative AI and storys. the AI will be fed prompts by the user and create custom stories. make it edgar allan poe themed. thanks ");
   console.log(response);
-  let response2 = await ChatGPT.ask("now add a dragon to the story");
+
+  let conversationId2 = "another conversation name";
+  let response2 = await bot.ask("Hello?", conversationId2);
   console.log(response2);
 
   app.listen(PORT, () => {
