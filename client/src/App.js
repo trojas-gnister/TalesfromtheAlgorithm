c3dd9ae32e07ded1ec112c6224f283b612f2cbd6
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-03-04 11:45:48 -0500

    added /home background image and created a /pages folder to hold the js file for all of our different webpages

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
         </Route>
       </Routes>
     </>
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,45 @@
+import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals'
 import App from './App'
 import './index.css'
 
+// Global Theme Settings
+import darkScrollbar from '@mui/material/darkScrollbar';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f2a2e8',
+    },
+    secondary: {
+      main: '#FFFFFF'
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: (themeParam) => ({
+        body: themeParam.palette.mode === 'dark' ? darkScrollbar() : null,
+      }),
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Arvo'
+    ]
+
+  },
+
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+
   document.getElementById('root')
 );
 
