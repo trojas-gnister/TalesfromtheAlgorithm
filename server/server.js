ad96917a55c6d52b468cdcca52ea8caea325839d
unknown <taskaleg@gmail.com>
2023-03-07 21:20:38 -0600

    resolvers file updated with chat

--- server/server.js
+++ server/server.js
@@ -64,3 +64,45 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
+
+const key = process.env.KEY;
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoDB_connect = require("./config/connection");
+
+// Set up middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Set up the ApolloServer middleware and start server once everything connects
+async function startServer() {
+  try {
+    await mongoDB_connect();
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+  } catch (err) {
+    console.error(err);
+  }
+}
+startServer();
+
+app.listen({ port: process.env.PORT || 3001 }, () => {
+  console.log(`
+    ==============================
+    "Online at ${PORT}, Server MEOW."
+
+            /\\_/\\
+            ( o o )
+            =( I )=
+              \/_
+
+    ==============================
+  `);
+});
