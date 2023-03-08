0da975181e80c0ad67e1c8af30d059dcc0576bd5
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-03-07 22:29:26 -0500

    made the threecards, have fun mitch kisses baby

--- client/src/components/ThreeCards.js
+++ client/src/components/ThreeCards.js
@@ -1,23 +1,87 @@
-import { Paper, Container } from '@mui/material'
+import {
+  Paper,
+  Container,
+  Grid,
+  Typography,
+  Box
+} from '@mui/material'
 
 export default function ThreeCards() {
   return (
     <Container maxWidth={false} disableGutters
     sx={{ alignItems: 'center'}}
     >
-    <Paper elevation={24}
+      <Grid container spacing={0.5}
+      sx={{ height: '30vh', marginTop: 40}}
+      >
+        <Paper elevation={24}
             sx={{
                 bgcolor: '#11153A',
                 paddingTop: 6,
                 paddingBottom: 6,
                 paddingLeft: 10,
-                width: '50%',
+                width: '23%',
                 marginTop: 5,
                 marginLeft: 'auto',
                 marginRight: 'auto',
                 borderRadius: 10,
+                border: 1,
+                borderColor: '#7DF9FF',
+                }}>
+                  <Typography variant='h5'
+                  sx={{
+                    marginBottom: 2,
+                  }}>
+                    Weiner
+                  </Typography>
+                  <Box
+                  sx={{
+                    position: 'left',
+                    width: 100,
+                    height: 5,
+                    bgcolor: '#7DF9FF',
+                  }}>
+
+                  </Box>
+
+
+
+                  <Typography variant='body1'>
+                    Body text
+                  </Typography>
+
+
+                </Paper>
+      <Paper elevation={24}
+            sx={{
+                bgcolor: '#11153A',
+                paddingTop: 6,
+                paddingBottom: 6,
+                paddingLeft: 10,
+                width: '23%',
+                marginTop: 5,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                borderRadius: 10,
+                border: 1,
+                borderColor: '#7DF9FF'
+                }}></Paper>
+      <Paper elevation={24}
+            sx={{
+                bgcolor: '#11153A',
+                paddingTop: 6,
+                paddingBottom: 6,
+                paddingLeft: 10,
+                width: '23%',
+                marginTop: 5,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                borderRadius: 10,
+                border: 1,
+                borderColor: '#7DF9FF'
                 }}></Paper>
 
+      </Grid>
     </Container>
 
   )
