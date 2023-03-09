ad560229890a11ee545cf59ddd8bef2b1a294908
iskry <refocuslucifer@protonmail.com>
2023-03-08 21:02:17 -0600

    added eventhandler and adjusted graphql queries

--- server/graphQL/resolvers.js
+++ server/graphQL/resolvers.js
@@ -1,16 +1,34 @@
-const { User } = require("../models");
-
-module.exports = {
-  Query: {
-    users: () => User.find(),
-  },
+const resolvers = {
   Mutation: {
-    addUser: async (_, { username, password }) => {
+    async login(_, { username, password }) {
+      // query MongoDB for user with matching email and password
+      const user = await User.findOne({ username, password });
+      
+      // if no user found, throw an error
+      if (!user) {
+        throw new Error('Invalid login credentials');
+      }
+      
+      // generate a JSON Web Token for the user
+      const token = generateToken(user);
+      
+      // return the token and user information as part of the response
+      return { token, user };
+    },
+    
+    async addUser(_, { username, password }) {
+      // create a new user object
       const user = new User({ username, password });
+      
+      // save the user to MongoDB
       await user.save();
-      return user;
+      
+      // generate a JSON Web Token for the user
+      const token = generateToken(user);
+      
+      // return the token and user information as part of the respon:se
+      return { token, user };
     },
   },
 };
 
-//  resolvers check what data is in db
