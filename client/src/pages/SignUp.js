cd80cc9618c09c84c65683e1efed712e2786e28c
iskry <refocuslucifer@protonmail.com>
2023-03-09 09:52:25 -0600

    updated signup.js

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import { useContext, useState } from 'react';
+import { authContext } from '../contexts/AuthContext';
+import { useForm} from "../utils/hooks";
+import { useMutation } from "@apollo/client";
+
+import { gql } from "graphql-tag"
+import { useNavigate } from 'react-router-dom';
+
 import { Container } from '@mui/material'
 import Background from '../assets/darkest-background.png';
 import SignUpForm from '../components/SignUpForm'
 import DetailedAppBar from '../components/DetailedAppBar';
+
+
+const REGISTER_USER = gql`
+        mutation Mutation(
+          $registerInput: RegisterInput!
+        ) {
+          registerUser(registerInput: $registerInput) {
+            email
+            username
+            token
+        }
+      }
+`
+
+function Register(props) {
+  const context = useContext(authContext);
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(registerUserCallback, {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [ registerUser, { loading } ] = useMutation(REGISTER_USER, {
+    update(proxy, { data: { registerUser: userData } }) {
+      context.login(userData);
+      navigate('/');
+    },
+    onError({ graphQLErrors }) {
+      setErrors(graphQLErrors);
+    },
+    variables: { registerInput: values }
+  });
+}
+
 
 export default function SignUp() {
     return (
@@ -23,3 +69,4 @@ export default function SignUp() {
         </Container>
     )
 }
+
